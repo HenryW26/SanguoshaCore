37a9d106ba22a3453014190c88ab9ce0f6e40414
picasso250 <cumt.xiaochi@gmail.com>
2013-04-09 09:01:26 +0800

    0

--- py/sanguosha_core.py
+++ py/sanguosha_core.py
@@ -12,15 +12,28 @@ class Game:
 
         # 分配身份
         self._players = [Player(i+1) for i in range(self.player_count)]
-        
+
+        i = 0
+        for id_type in self._id_types:
+            for z in range(self._id_types[id_type]):
+                self._players[i].type = id_type
+                i = i + 1
 
         # 随机位置
+    def id_types(self, id_types):
+        self._id_types = id_types
+    def player(self, i):
+        return self._players[i]
+    
     def king_roles(self):
         id_type = 'king'
         return {"player": self.players(id_type)[0], "roles": self.make_roles(id_type)}
     def players(self, id_type):
         return [p for p in self._players if p.type == id_type]
-             
+    def make_roles(self, id_type):
+        if id_type == 'king':
+            return {'player': self._players[0], 'roles': [Role('zhenji') for i in range(5)]}
+        return [Role('zhe') for i in range(3)]
     def start(self):
         pass
         # 身份
@@ -40,10 +53,16 @@ def game_end():
 
 class Player:
     """docstring for Player"""
-    def __init__(self, n):
-        self.n = n
+    def __init__(self, index):
+        self.index = index
     def action(self):
         pass
+
+class Role:
+    """docstring for Role"""
+    def __init__(self, name):
+        self.name = name
+        
             
 # test    
 if __name__ == '__main__':
@@ -54,25 +73,26 @@ if __name__ == '__main__':
 
     game = Game()
     game.player_count = 5
-    game.king_count = 1
-    game.loyal_count = 1
-    game.cheat_count = 1
-    game.rebel_count = 2
+    game.id_types({'king': 1, 'loyal': 1, 'sinister': 1, 'rebel': 2})
 
     game.init() # 洗牌 分配身份
 
     # 主公选将
-    king = game.king_roles() # return king player and roles
-    show_game_roles(king)
-    role_id = get_user_input_role_select()
-    king.player.role_selector = role_id
+    king = game.players('king')
+    roles = game.select_roles(king)
+    print(roles)
+    show_game_roles(roles)
 
-    # # 其他人选将
-    # player = game.player_roles()
-    # while player:
-    #     show_game_roles(player)
-    #     role_id = get_user_input_role_select()
-    #     player.player.role_selector = role_id
+    role_id = get_user_input_role_select()
+    print(role_id)
+    king.role_selector = role_id
+
+    # 其他人选将
+    players = game.players('other')
+    while player:
+        show_game_roles(player)
+        role_id = get_user_input_role_select()
+        player.player.role_selector = role_id
 
     # game.start(king.player) # 从主公开始出牌
 
